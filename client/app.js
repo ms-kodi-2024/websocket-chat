abb9c41f5f7c1623d2365d761e38f43ca7bf6c89
Marcin Stalmach <marcin.stalmach123@outlook.com>
2025-03-30 10:43:09 +0200

    Add support for adding messages

--- client/app.js
+++ client/app.js
@@ -20,3 +20,29 @@ const login = (e) => {
 };
 
 loginForm.addEventListener('submit', login);
+
+function addMessage(author, content) {
+  const message = document.createElement('li');
+  message.classList.add('message', 'message--received');
+  if (author === userName) message.classList.add('message--self');
+  message.innerHTML = `
+    <h3 class="message__author">${author === userName ? 'You' : author}</h3>
+    <div class="message__content">
+      ${content}
+    </div>
+  `;
+  messagesList.appendChild(message);
+}
+
+const sendMessage = (e) => {
+  e.preventDefault();
+  const messageContent = messageContentInput.value.trim();
+  if (messageContent === '') {
+    alert('You must write something!');
+  } else {
+    addMessage(userName, messageContent);
+    messageContentInput.value = '';
+  }
+};
+
+addMessageForm.addEventListener('submit', sendMessage);
